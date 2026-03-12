a3cdff6894459426dee01827d8d5ed43431e6663
nusratnova5 <nusrat.nova16@gmail.com>
2026-03-12 12:05:27 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    generateBuildId: async () => {
+        return `${Date.now()}`
+    },
+
     async headers() {
         return [
           {
