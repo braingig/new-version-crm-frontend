ea2a8ff44de86d144dd4ea57b93da8d24902cba6
nusratnova5 <nusrat.nova16@gmail.com>
2026-03-12 11:39:29 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    async headers() {
+        return [
+          {
+            source: "/:path*",
+            headers: [
+              {
+                key: "Cache-Control",
+                value: "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
+              },
+            ],
+          },
+        ];
+      },
 }
 
 module.exports = nextConfig
